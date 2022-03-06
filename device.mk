291a4790aa02e867960c532990d9a8478f9257fe
Alexander Koskovich <akoskovich@pm.me>
2022-03-06 02:11:47 -0700

    hiphi: Update to Boot Control HAL v1.2
    Signed-off-by: 7Soldier <reg.fm4@gmail.com>

--- device.mk
+++ device.mk
@@ -30,9 +30,9 @@ AB_OTA_POSTINSTALL_CONFIG += \
 
 # Boot control HAL
 PRODUCT_PACKAGES += \
-    android.hardware.boot@1.1-impl-qti \
-    android.hardware.boot@1.1-impl-qti.recovery \
-    android.hardware.boot@1.1-service
+    android.hardware.boot@1.2-impl-qti \
+    android.hardware.boot@1.2-impl-qti.recovery \
+    android.hardware.boot@1.2-service
 
 PRODUCT_PACKAGES_DEBUG += \
     bootctl
