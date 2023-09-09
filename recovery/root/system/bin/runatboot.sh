f2a474f795cb6426c78838c4027e4056fcb805d0
lexingtontezz <tezz.dixon@outlook.com>
2023-09-09 00:08:43 -0400

    touchscreen: support ilitek touch panels
    Change-Id: I2c0753c219143657b9ebc9508872c29e48ab852d

--- recovery/root/system/bin/runatboot.sh
+++ recovery/root/system/bin/runatboot.sh
@@ -1,11 +1,17 @@
 #!/system/bin/sh
+
 mount /vendor_dlkm
 mount /vendor
 modprobe -d /vendor/lib/modules /vendor/lib/modules/nova_0flash_mmi.ko
+modprobe -d /vendor/lib/modules /vendor/lib/modules/ili9882_mmi.ko
 sleep 1
+if [ $(cat /sys/class/touchscreen/primary/productinfo) == "NT36672C" ]
+then
 echo 1 > /sys/class/touchscreen/primary/forcereflash
 echo novatek_ts-csot-NT36672C-2302240D-605b-genevn.bin > /sys/class/touchscreen/primary/doreflash
+echo "Reflashing firmware..."
 echo 0 > /sys/class/touchscreen/primary/forcereflash
+fi
 
 is_fastboot=$(getprop init.svc.fastbootd)
 if [ "$is_fastboot" != "running" ]; then
